cb7e032622e40b20f3b4792ec2b0c1b07beeb5d1
dash <dashkernel@gmail.com>
2018-12-06 23:12:13 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://8.0.0.130:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:D4t4b453-123456@ds127954.mlab.com:27954/cafe-01';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
